38788cbf9494d5d9f24a33feb619542684201181
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2022-05-22 08:32:13 +0200

    first commit

---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class SmallText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  SmallText({
+    Key? key,
+    this.color = const Color(0xFF564F4D),
+    required this.text,
+    this.size = 12,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+      ),
+    );
+  }
+}
